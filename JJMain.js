6a300ef82094d6bdd724073cf7335522973816d9
R. Scott Thompson <scobes@jamporium.com>
2014-07-05 14:47:36 -0700

    Jumping Jacks Archive - initial

---
+++ JJMain.js
@@ -0,0 +1,183 @@
+// JavaScript Document
+ 
+var fadeimages=new Array()
+//SET IMAGE PATHS. Extend or contract array as needed
+fadeimages[0]=["images/jjgeneral/other/promo_tournament.jpg", "http://www.jumpingjacksparty.com/rancho_cucamonga/rancho_otherstuff.html", ""] //plain image syntax
+fadeimages[1]=["images/jjgeneral/other/promo_40discount_sept.jpg", "http://www.jumpingjacksparty.com/rancho_cucamonga/rancho_otherstuff.html", ""] //image with link syntax
+//fadeimages[2]=["photo3.jpg", "http://www.jumpingjacksparty.com", "_new"] //image with link and target syntax
+ 
+var fadebgcolor=""
+ 
+////NO need to edit beyond here/////////////
+ 
+var fadearray=new Array() //array to cache fadeshow instances
+var fadeclear=new Array() //array to cache corresponding clearinterval pointers
+ 
+var dom=(document.getElementById) //modern dom browsers
+var iebrowser=document.all
+ 
+function fadeshow(theimages, fadewidth, fadeheight, borderwidth, delay, pause, displayorder){
+this.pausecheck=pause
+this.mouseovercheck=0
+this.delay=delay
+this.degree=10 //initial opacity degree (10%)
+this.curimageindex=0
+this.nextimageindex=1
+fadearray[fadearray.length]=this
+this.slideshowid=fadearray.length-1
+this.canvasbase="canvas"+this.slideshowid
+this.curcanvas=this.canvasbase+"_0"
+if (typeof displayorder!="undefined")
+theimages.sort(function() {return 0.5 - Math.random();}) //thanks to Mike (aka Mwinter) :)
+this.theimages=theimages
+this.imageborder=parseInt(borderwidth)
+this.postimages=new Array() //preload images
+for (p=0;p<theimages.length;p++){
+this.postimages[p]=new Image()
+this.postimages[p].src=theimages[p][0]
+}
+ 
+var fadewidth=fadewidth+this.imageborder*2
+var fadeheight=fadeheight+this.imageborder*2
+ 
+if (iebrowser&&dom||dom) //if IE5+ or modern browsers (ie: Firefox)
+document.write('<div id="master'+this.slideshowid+'" style="position:relative;width:'+fadewidth+'px;height:'+fadeheight+'px;overflow:hidden;"><div id="'+this.canvasbase+'_0" style="position:absolute;width:'+fadewidth+'px;height:'+fadeheight+'px;top:0;left:0;filter:progid:DXImageTransform.Microsoft.alpha(opacity=10);-moz-opacity:10;-khtml-opacity:10;background-color:'+fadebgcolor+'"></div><div id="'+this.canvasbase+'_1" style="position:absolute;width:'+fadewidth+'px;height:'+fadeheight+'px;top:0;left:0;filter:progid:DXImageTransform.Microsoft.alpha(opacity=10);-moz-opacity:10;background-color:'+fadebgcolor+'"></div></div>')
+else
+document.write('<div><img name="defaultslide'+this.slideshowid+'" src="'+this.postimages[0].src+'"></div>')
+ 
+if (iebrowser&&dom||dom) //if IE5+ or modern browsers such as Firefox
+this.startit()
+else{
+this.curimageindex++
+setInterval("fadearray["+this.slideshowid+"].rotateimage()", this.delay)
+}
+}
+
+function fadepic(obj){
+if (obj.degree<100){
+obj.degree+=10
+if (obj.tempobj.filters&&obj.tempobj.filters[0]){
+if (typeof obj.tempobj.filters[0].opacity=="number") //if IE6+
+obj.tempobj.filters[0].opacity=obj.degree
+else //else if IE5.5-
+obj.tempobj.style.filter="alpha(opacity="+obj.degree+")"
+}
+else if (obj.tempobj.style.MozOpacity)
+obj.tempobj.style.MozOpacity=obj.degree/101
+else if (obj.tempobj.style.KhtmlOpacity)
+obj.tempobj.style.KhtmlOpacity=obj.degree/100
+}
+else{
+clearInterval(fadeclear[obj.slideshowid])
+obj.nextcanvas=(obj.curcanvas==obj.canvasbase+"_0")? obj.canvasbase+"_0" : obj.canvasbase+"_1"
+obj.tempobj=iebrowser? iebrowser[obj.nextcanvas] : document.getElementById(obj.nextcanvas)
+obj.populateslide(obj.tempobj, obj.nextimageindex)
+obj.nextimageindex=(obj.nextimageindex<obj.postimages.length-1)? obj.nextimageindex+1 : 0
+setTimeout("fadearray["+obj.slideshowid+"].rotateimage()", obj.delay)
+}
+}
+ 
+fadeshow.prototype.populateslide=function(picobj, picindex){
+var slideHTML=""
+if (this.theimages[picindex][1]!="") //if associated link exists for image
+slideHTML='<a href="'+this.theimages[picindex][1]+'" target="'+this.theimages[picindex][2]+'">'
+slideHTML+='<img src="'+this.postimages[picindex].src+'" border="'+this.imageborder+'px">'
+if (this.theimages[picindex][1]!="") //if associated link exists for image
+slideHTML+='</a>'
+picobj.innerHTML=slideHTML
+}
+ 
+
+ 
+fadeshow.prototype.rotateimage=function(){
+if (this.pausecheck==1) //if pause onMouseover enabled, cache object
+var cacheobj=this
+if (this.mouseovercheck==1)
+setTimeout(function(){cacheobj.rotateimage()}, 100)
+else if (iebrowser&&dom||dom){
+this.resetit()
+var crossobj=this.tempobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+crossobj.style.zIndex++
+fadeclear[this.slideshowid]=setInterval("fadepic(fadearray["+this.slideshowid+"])",50)
+this.curcanvas=(this.curcanvas==this.canvasbase+"_0")? this.canvasbase+"_1" : this.canvasbase+"_0"
+}
+else{
+var ns4imgobj=document.images['defaultslide'+this.slideshowid]
+ns4imgobj.src=this.postimages[this.curimageindex].src
+}
+this.curimageindex=(this.curimageindex<this.postimages.length-1)? this.curimageindex+1 : 0
+}
+ 
+fadeshow.prototype.resetit=function(){
+this.degree=10
+var crossobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+if (crossobj.filters&&crossobj.filters[0]){
+if (typeof crossobj.filters[0].opacity=="number") //if IE6+
+crossobj.filters(0).opacity=this.degree
+else //else if IE5.5-
+crossobj.style.filter="alpha(opacity="+this.degree+")"
+}
+else if (crossobj.style.MozOpacity)
+crossobj.style.MozOpacity=this.degree/101
+else if (crossobj.style.KhtmlOpacity)
+crossobj.style.KhtmlOpacity=obj.degree/100
+}
+ 
+ 
+fadeshow.prototype.startit=function(){
+var crossobj=iebrowser? iebrowser[this.curcanvas] : document.getElementById(this.curcanvas)
+this.populateslide(crossobj, this.curimageindex)
+if (this.pausecheck==1){ //IF SLIDESHOW SHOULD PAUSE ONMOUSEOVER
+var cacheobj=this
+var crossobjcontainer=iebrowser? iebrowser["master"+this.slideshowid] : document.getElementById("master"+this.slideshowid)
+crossobjcontainer.onmouseover=function(){cacheobj.mouseovercheck=1}
+crossobjcontainer.onmouseout=function(){cacheobj.mouseovercheck=0}
+}
+this.rotateimage()
+}
+ 
+</script>
+<style type="text/css">
+<!--
+body {
+	background-color: #FFFFFF;
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+-->
+</style>
+<!-- ImageReady Preload Script (JumpingJacks.psd) -->
+<script type="text/javascript">
+<!--
+
+function newImage(arg) {
+	if (document.images) {
+		rslt = new Image();
+		rslt.src = arg;
+		return rslt;
+	}
+}
+
+function changeImages() {
+	if (document.images && (preloadFlag == true)) {
+		for (var i=0; i<changeImages.arguments.length; i+=2) {
+			document[changeImages.arguments[i]].src = changeImages.arguments[i+1];
+		}
+	}
+}
+
+var preloadFlag = false;
+function preloadImages() {
+	if (document.images) {
+		JumpingJacks_whatis_over = newImage("images/jjgeneral/other/JumpingJacks_whatis-over.jpg");
+		JumpingJacks_schedule_over = newImage("images/jjgeneral/other/JumpingJacks_schedule-over.jpg");
+		JumpingJacks_locations_over = newImage("images/jjgeneral/other/JumpingJacks_locations-over.jpg");
+		JumpingJacks_photos_over = newImage("images/jjgeneral/other/JumpingJacks_photos-over.jpg");
+		JumpingJacks_contact_over = newImage("images/jjgeneral/other/JumpingJacks_contact-over.jpg");
+		JumpingJacks_FAQ_over = newImage("images/jjgeneral/other/JumpingJacks_FAQ-over.jpg");
+		JumpingJacks_NotJustKids_over = newImage("images/jjgeneral/other/JumpingJacks_NotJustKids-ov.jpg");
+		preloadFlag = true;
+	}
+}
---
+++ JJgeneral/Scheduler/Scheduler.js
@@ -0,0 +1,171 @@
+﻿// JScript File
+// global variables
+var StoreLocation;
+
+function GetDate(CtrlName)
+{
+/****************************************************
+
+Use Javascript method (window.open) to PopUp a new window 
+which contain a Calendar Control. In the meantime, we'll 
+pass the Parent Form Name and Request Control Name in the QueryString!
+
+*****************************************************/
+
+ChildWindow = window.open('Calendar.aspx?FormName=' + document.forms[0].name + '&CtrlName=' + CtrlName, "PopUpCalendar", "width=270,height=300,top=200,left=200,toolbars=no,scrollbars=no,status=no,resizable=no");
+}
+
+function CheckWindow()
+{
+ChildWindow.close();
+}
+
+function newImage(arg) {
+	if (document.images) {
+		rslt = new Image();
+		rslt.src = arg;
+		return rslt;
+	}
+}
+function initialize(){
+    preloadImages();
+    localize();
+}
+
+function ccinitialize(){
+    preloadImages();
+    cclocalize();   
+}
+
+function changeImages() {
+	if (document.images && (preloadFlag == true)) {
+		for (var i=0; i<changeImages.arguments.length; i+=2) {
+			document[changeImages.arguments[i]].src = changeImages.arguments[i+1];
+		}
+	}
+}
+function getlocationfromurl(){
+    //this function looks for loc= in the calling url,
+    //then pieces together the two letters representing the store location
+    temp = new String(location.search);
+    stringindex = temp.indexOf("loc=",0)    
+    //letter1 = temp.charAt(stringindex+4);
+    letter1 = 'r'
+    //letter2 = temp.charAt(stringindex+5);
+    letter2 = 'c'
+    StoreLocation = letter1 + letter2;
+    //alert(StoreLocation);
+}
+
+function localize(){
+
+	//get store location code from URL
+	getlocationfromurl();
+		
+	//These are the header image and the top image of the side menu with store location in text
+	switch(StoreLocation){
+	    case "rc":
+	        //first localize the necessary graphics...
+	        document['HeaderGraphic'].src="../../images/ranchocucamonga/graphics/header_loc_rc.jpg"
+	        document['SideMenuHeader'].src = "../../images/ranchocucamonga/graphics/menus/smenu_rc1.jpg"
+	        //These next five will probably be the same for all locations, but we'll do 'em this way
+	        //any way
+	        document['maplinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_map.jpg"
+	        document['optionslinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_options.jpg"
+	        document['schedulerlinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_schedule-over.jpg"
+	        document['photolinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_photo.jpg"
+	        document['downloadlinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_download.jpg"
+	        //Now we change the accompanying links to match the store location...
+	        document.getElementById('maplink').href="../../rancho_cucamonga/rancho_locmap.html"
+	        document.getElementById('optionslink').href="../../rancho_cucamonga/rancho_options.html"
+	        document.getElementById('schedulerlink').href="DateSchedule.aspx?loc=rc"
+	        document.getElementById('photoslink').href="../../rancho_cucamonga/rancho_photos.html"
+	        document.getElementById('downloadlink').href="../../rancho_cucamonga/rancho_otherstuff.html"
+	    default:
+	}
+	
+	SetCCBankAccount();
+	
+}
+
+function cclocalize(){
+
+	//get store location code from URL
+	getlocationfromurl();
+		
+	//These are the header image and the top image of the side menu with store location in text
+	switch(StoreLocation){
+	    case "rc":
+	        //first localize the necessary graphics...
+	        document['HeaderGraphic'].src="../../images/ranchocucamonga/graphics/header_loc_rc.jpg"
+	        document['SideMenuHeader'].src = "../../images/ranchocucamonga/graphics/menus/smenu_rc1.jpg"
+	        //These next five will probably be the same for all locations, but we'll do 'em this way
+	        //any way
+	        document['maplinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_map.jpg"
+	        document['optionslinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_options.jpg"
+	        document['schedulerlinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_schedule-over.jpg"
+	        document['photolinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_photo.jpg"
+	        document['downloadlinkimage'].src="../../images/ranchocucamonga/graphics/menus/smenu_rc_download.jpg"
+	        
+	        //Now we change the accompanying links to match the store location...
+	        document.getElementById('maplink').href="../../rancho_cucamonga/rancho_locmap.html"
+	        document.getElementById('optionslink').href="../../rancho_cucamonga/rancho_options.html"
+	        document.getElementById('schedulerlink').href="DateSchedule.aspx?loc=rc"
+	        document.getElementById('photoslink').href="../../rancho_cucamonga/rancho_photos.html"
+	        document.getElementById('downloadlink').href="../../rancho_cucamonga/rancho_otherstuff.html"
+	            
+            //lastly, we gotta strip off URL encoding so LinkPoint will process the credit card
+	        //location.replace("processcreditcardstart.aspx")
+	                
+	    default:
+	}
+	
+	SetCCBankAccount();	
+
+
+}
+function SetCCBankAccount(){
+	//if this is the form to start credit card processing, set the Verisign
+	//login variables
+	    if (document.forms[0].id == "VerisignForm1"){
+	    	//alert(document.forms[0].id);
+	        switch (StoreLocation){
+	            case "rc":
+                    document.forms[0].Hidden1.value = "jum4813087379"
+                    document.forms[0].Hidden2.value = "wfb"
+                default:
+            }
+         }
+}
+
+var preloadFlag = false;
+function preloadImages() {
+	if (document.images) {
+		JTop_home_over = newImage("../../images/JTop_home-over.jpg");
+		JTop_whatis_over = newImage("../../images/JTop_whatis-over.jpg");
+		JTop_sched_over = newImage("../../images/JTop_sched-over.jpg");
+		JTop_location_over = newImage("../../images/JTop_location-over.jpg");
+		JTop_gallery_over = newImage("../../images/JTop_gallery-over.jpg");
+		preloadFlag = true;
+	}	
+}
+
+var popUp; 
+
+function OpenCalendar(idname, postBack)
+{
+	popUp = window.open('calendar.aspx?formname=' + document.forms[0].name + 
+		'&id=' + idname + '&selected=' + document.forms[0].elements[idname].value + '&postBack=' + postBack, 
+		'popupcal', 
+		'width=170,height=240,left=200,top=250');
+}
+
+function SetDate(formName, id, newDate, postBack)
+{
+	eval('var theform = document.' + formName + ';');
+	popUp.close();
+	theform.elements[id].value = newDate;
+	if (postBack)
+		__doPostBack(id,'');
+}		
+
